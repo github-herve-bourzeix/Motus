978c324c859b642ed09f97febc3890e52545d2ab
Hervé Bourzeix <herve.bourzeix@teads.tv>
2018-10-06 18:38:23 +0200

    add theme support

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import words from './words'
+import apolline from './theme/apolline'
+import nombres from './theme/nombres'
+import pokemon from './theme/pokemon'
+import zelda from './theme/zelda'
 import _ from 'lodash'
 
 import './styles.css'
 
-const choisirMot = (max = 5) => {
-  const filteredWords = words.filter(word => word.length <= max)
+const choisirMot = (max = 5, theme = 'Apolline') => {
+  console.info('selected theme:', theme)
+  let themeWords = words
+  if (theme === 'Apolline') {
+    themeWords = apolline
+  } else if (theme === 'Les nombres') {
+    themeWords = nombres
+  } else if (theme === 'Pokemon') {
+    themeWords = pokemon
+  } else if (theme === 'Zelda') {
+    themeWords = zelda
+  }
+
+  const filteredWords = themeWords.filter(word => word.length <= max)
   const index = Math.floor(Math.random() * filteredWords.length)
   return filteredWords[index]
 }
@@ -23,19 +39,19 @@ const StyleLettre = {
   marginRight: '10px',
   marginBottom: '10px',
   background: 'grey',
-  borderRadius: '10px'
+  borderRadius: '10px',
 }
 
 const LettreOk = {
-  background: 'green'
+  background: 'green',
 }
 
 const LettreKo = {
-  background: 'red'
+  background: 'red',
 }
 
 const LettreMalPlacee = {
-  background: 'blue'
+  background: 'blue',
 }
 
 // const Lettre = props => <input type="text" {...props} />
@@ -100,14 +116,36 @@ const MasqueDeSaisie = ({
   )
 }
 
-class App extends Component {
-  constructor() {
-    super()
+class MainBoard extends Component {
+  constructor(props) {
+    super(props)
     this.state = this.getInitialState()
   }
 
-  getInitialState() {
-    const motChoisi = choisirMot()
+  getMaxLengthFromLevel = niveau => {
+    switch (niveau) {
+      case 'moyen':
+        return 7
+      case 'cauchemar':
+        return Number.POSITIVE_INFINITY
+      case 'facile ':
+      default:
+        return 5
+    }
+  }
+
+  componentWillReceiveProps(props) {
+    this.setState(this.getInitialState(props))
+  }
+
+  handleConfigure = () => {
+    this.props.onConfigure()
+  }
+
+  getInitialState(props) {
+    const { niveau, theme } = (props || this.props).configuration
+
+    const motChoisi = choisirMot(this.getMaxLengthFromLevel(niveau), theme)
     const aideIndex = Math.floor(Math.random() * motChoisi.length)
     const motSaisi = []
     motSaisi[aideIndex] = motChoisi.split('')[aideIndex]
@@ -121,12 +159,12 @@ class App extends Component {
       motSaisi,
       resultat,
       lettreMauvaises: [],
-      partieGagnee: false
+      partieGagnee: false,
     }
   }
 
   restart = () => {
-    this.setState(this.getInitialState)
+    this.setState(() => this.getInitialState())
   }
 
   validerSaisie = () => {
@@ -135,7 +173,7 @@ class App extends Component {
     const isLocaleEq = (x, y) => {
       return (
         x.localeCompare(y, 'en', {
-          sensitivity: 'base'
+          sensitivity: 'base',
         }) === 0
       )
     }
@@ -204,7 +242,7 @@ class App extends Component {
       partieGagnee,
       resultat: monResultat,
       nombreEssai: nombreEssai + 1,
-      lettreMauvaises: lettreInvalides
+      lettreMauvaises: lettreInvalides,
     })
   }
 
@@ -222,19 +260,15 @@ class App extends Component {
       resultat,
       nombreEssai,
       lettreMauvaises,
-      niveau
+      niveau,
     } = this.state
 
     console.info('lettreMauvaises', lettreMauvaises)
     return (
       <div className="App">
-        <h1>
-          Nombre d'essai:{nombreEssai} {lettreMauvaises}
-        </h1>
-        <input type="radio" value="facile" checked={niveau === 'facile'} />{' '}
-        facile
-        <input type="radio" value="facile" /> moyen
-        <input type="radio" value="facile" /> cauchemar
+        <input type="button" value="Configure" onClick={this.handleConfigure} />
+        <h1>Nombre d'essai:{nombreEssai}</h1>
+
         <h2>Essaye de trouver le mot!</h2>
         <h2>
           <MasqueDeSaisie
@@ -242,13 +276,13 @@ class App extends Component {
             mot={lettreMauvaises.join('')}
             saisie={lettreMauvaises.join('')}
             maxLength="1"
-            value={lettreMauvaises.map(x => 0)}
+            value={lettreMauvaises.map(x => 1)}
             style={Object.assign({}, StyleLettre, {
               height: '30px',
               width: '30px',
               fontSize: '25px',
               borderRadius: '90px',
-              backgroundColor: 'purple'
+              backgroundColor: 'purple',
             })}
           />
         </h2>
@@ -262,8 +296,17 @@ class App extends Component {
               value={resultat}
               onChange={this.gererChangement}
             />
-            <input type="button" value="Valider" onClick={this.validerSaisie} />
-            <input type="button" value="Recommencer" onClick={this.restart} />
+            <input
+              type="button"
+              value="Valider"
+              onClick={this.validerSaisie}
+              style={{
+                width: '200px',
+                lineHeight: '299px',
+                fontSize: '60px',
+                height: '200px',
+              }}
+            />
           </div>
         )}
         {partieGagnee && (
@@ -273,6 +316,130 @@ class App extends Component {
           </div>
         )}
       </div>
+    )
+  }
+}
+
+const Dialog = ({ children, open = true }) => (
+  <div style={{ display: open ? '' : 'none' }}>
+    <div
+      style={{
+        position: 'absolute',
+        zIndex: 1,
+        height: '100vh',
+        width: '100vw',
+        opacity: '0.3',
+        backgroundColor: 'black',
+      }}
+    />
+    <div
+      style={{
+        position: 'absolute',
+        display: 'flex',
+        justifyContent: 'center',
+        zIndex: 2,
+        width: '100vw',
+        height: '100vh',
+      }}
+    >
+      <div
+        style={{
+          justifyContent: 'center',
+          display: 'flex',
+          marginTop: 'auto',
+          marginBottom: 'auto',
+          width: '75vw',
+          background: 'white',
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  </div>
+)
+
+const Niveau = ({ name, ...props }) => (
+  <div
+    style={{
+      display: 'flex',
+      flexDirection: 'row',
+    }}
+  >
+    <input name="niveau" type="radio" defaultValue={name} {...props} />
+    <span style={{ marginLeft: '10px', marginBottom: '10px' }}>{name}</span>
+  </div>
+)
+
+class Settings extends Component {
+  onSave = () => {
+    const { onSave } = this.props
+    onSave({
+      niveau: this.formRef['niveau'].value,
+      theme: this.formRef['theme'].value,
+    })
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          padding: '20px',
+          fontFamily: 'helvetica',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <h2>Configure tes paramètres</h2>
+        <form ref={ref => (this.formRef = ref)}>
+          <Niveau name="facile" checked />
+          <Niveau name="moyen" />
+          <Niveau name="difficile" />
+          <h3>Choisie un thème:</h3>
+          <select name="theme" defaultValue="Apolline">
+            <option>Zelda</option>
+            <option>Pokemon</option>
+            <option>Les nombres</option>
+            <option>Apolline</option>
+          </select>
+          <hr />
+        </form>
+        <input type="button" value="ok" onClick={this.onSave} />
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  state = {
+    isSettingsDialogOpened: true,
+    configuration: {},
+  }
+  handleOnSave = configuration => {
+    console.info('configuration', configuration)
+
+    this.setState({
+      isSettingsDialogOpened: false,
+      configuration,
+    })
+  }
+
+  handleConfigure = () => {
+    this.setState({
+      isSettingsDialogOpened: true,
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <Dialog open={this.state.isSettingsDialogOpened}>
+          <Settings onSave={this.handleOnSave} />
+        </Dialog>
+        <MainBoard
+          onConfigure={this.handleConfigure}
+          configuration={this.state.configuration}
+        />
+      </>
     )
   }
 }
---
+++ src/theme/pokemon.js
@@ -0,0 +1,1 @@
+export default ['pikatchu', 'pokeball']
